bd543ec996202230c0069508e63c83b70774b8d0
sharedordaz <sharedordaz@live.com.mx>
2021-10-28 21:30:57 -0500

    finished 4 today

--- speed/game/words.py
+++ speed/game/words.py
@@ -1,11 +1,12 @@
 import random
+import os
 class Words:
     def __init__(self):
         self.words_list = []
 
     def importing(self):
         """Import the words.txt file and creates a list with the words in the file"""
-        with open("words.txt", "r") as words_file:
+        with open("words.txt", "rt") as words_file:
             for word in words_file:
                 self.words_list.append(word)
 
@@ -14,3 +15,10 @@ class Words:
         lista = self.words_list
         word = random.choice(lista)
         return word
+
+
+"""
+print (os.listdir())
+xd = Words()
+xd.importing()
+print (xd.ran_word())"""
