7aa75c89c36bb0590502c01455d35e091691e64a
sharedordaz <sharedordaz@live.com.mx>
2021-10-30 23:13:36 -0500

    empty commit

--- speed/__main__.py
+++ speed/__main__.py
@@ -4,6 +4,7 @@ from game.director import Director
 def main(screen):
     director = Director(screen)
     director.start_game()
+    input()
 
 print("Welcome to the speed game:\n\nYou have to see the words and writte them\n")
 input("Press ENTER twice to start playing")
