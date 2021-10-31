a8dcdac20296241c6b3972164e26818c97f6ae3b
sharedordaz <sharedordaz@live.com.mx>
2021-10-30 21:06:16 -0500

    can show letters

--- speed/game/output_service.py
+++ speed/game/output_service.py
@@ -7,11 +7,24 @@ from time import sleep
 class OutputService:
     def __init__(self):
         pass
-    def print_words(self, word, coordi):
+    
+    def print_words(self, words, coords):
         with ManagedScreen() as screen:
-            screen.print_at(word, coordi[0], coordi[1])
+            self.screen = screen
+            num = 0
+            for coord in coords:
+                screen.print_at(words[num], coord[0], coord[1])
+                num += 1
+            
             screen.refresh()
-            sleep(10)
+            sleep(99999)
+            
+    def refrescar(self):
+        self.screen.refresh()
+
+    def sleep(self, time):
+        sleep(time)
+            
 
 #os = OutputService()
 #os.print_words("hello world", (1,2))
